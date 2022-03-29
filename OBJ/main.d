eecd8861f4bb54da1a0a00f9ce20fbfa00c09173
dt <1816306195@qq.com>
2022-03-29 09:37:13 +0800

    串口发数据控制功能完成

--- OBJ/main.d
+++ OBJ/main.d
@@ -33,3 +33,5 @@
 ..\obj\main.o: ..\HARDWARE\LED\led.h
 ..\obj\main.o: ..\HARDWARE\BEEP\beep.h
 ..\obj\main.o: ..\HARDWARE\KEY\key.h
+..\obj\main.o: ..\HARDWARE\MYUSTAR\myusart.h
+..\obj\main.o: D:\keil_v5\MDK\ARM\ARMCC\Bin\..\include\stdio.h
--- USER/main.c
+++ USER/main.c
@@ -3,15 +3,18 @@
 #include "led.h"
 #include "beep.h"
 #include "key.h"
+#include "myusart.h"
 
 int main(void)
 {
 	u8 key=0;	
 	
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
 	delay_init();	    	//初始化延时函数	  
 	LED_Init();				//初始化LED
 	BEEP_Init();         	//初始化蜂鸣器
-	KEY_Init();         	//初始化按键
+	KEY_Init();            	//初始化按键
+	myuart_init(9600);		//串口初始化为9600
 	while(1)
 	{
  		key=KEY_Scan(0);	//得到键值
@@ -20,17 +23,24 @@ int main(void)
 			switch(key)
 			{				 
 				case WKUP_PRES:	//控制蜂鸣器
-					LED0=!LED0;
+					printf("OKK");
 					break; 
 				case KEY1_PRES:	//控制LED1翻转	 
 					LED1=!LED1;
 					break;
-				case KEY0_PRES:	//同时控制LED0,LED1翻转 
+				case KEY0_PRES:	//控制LED0翻转 
 					
-					LED1=!LED1;
+					LED0=!LED0;
 					break;
 			}
-		}else delay_ms(10); 
-	}	 
+		}
+	if (USART_RX_STA==0x8000)
+	{
+		USART_RX_STA=0;
+		if(USART_RX_BUF[0]==0xAA)	LED0=!LED0;
+	}
+}		
+			
+		 
 }
 
